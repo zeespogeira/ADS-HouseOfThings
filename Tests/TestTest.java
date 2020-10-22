9f50336afff66e69771c5b41c667ae6487e1357c
Jose Espogeira <jose.espogeira@outlook.com>
2020-10-22 18:59:45 +0100

    rearrenged folders

--- Tests/TestTest.java
+++ Tests/TestTest.java
@@ -1,6 +1,5 @@
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.mockito.BDDMockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -9,11 +8,6 @@ import static org.mockito.BDDMockito.*;
 
 
 public class TestTest {
-    //@BDDMockito
-    public UnitUnderTest databaseMock;
-    @ClassRule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
-    //BDDMockito.Then;
     @Test
     public void test1() {
         UnitUnderTest i = new UnitUnderTest();
